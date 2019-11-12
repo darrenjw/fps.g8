696b1ba3a1e0690991485f6af4c645c5124b9ffc
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2019-11-12 10:15:03 +0000

    test spec

--- src/main/g8/src/test/scala/$name$-test.scala
+++ src/main/g8/src/test/scala/$name$-test.scala
@@ -1,13 +1,14 @@
+import org.scalatest._
 import org.scalatest.flatspec.AnyFlatSpec
 
-class CatsSpec extends AnyFlatSpec {
+class CatsSpec extends AnyFlatSpec with Matchers {
 
   import cats._
   import cats.implicits._
 
  "A List" should "combine" in {
    val l = List(1,2) |+| List(3,4)
-    assert(l == List(1,2,3,4))
+   l should be (List(1,2,3,4))
   }
 
 }
