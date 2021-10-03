e635ec51c8e4acfb61fe4e72800f26fd00087815
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2021-10-03 16:12:55 +0100

    update to test syntax

--- src/main/g8/src/test/scala/$name$-test.scala
+++ src/main/g8/src/test/scala/$name$-test.scala
@@ -1,27 +1,22 @@
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
+import cats.*
+import cats.implicits.*
 
 // Example unit tests
-class CatsSpec extends AnyFlatSpec with Matchers {
-
-  import cats._
-  import cats.implicits._
+class MyUnitTests extends AnyFlatSpec with Matchers:
 
   "A List" should "combine" in {
     val l = List(1,2) |+| List(3,4)
     l should be (List(1,2,3,4))
   }
 
-}
-
 
 // Example property-based tests
-import org.scalatestplus.scalacheck._
-class MyPropertyTests extends AnyFlatSpec with Matchers with ScalaCheckPropertyChecks {
+import org.scalatestplus.scalacheck.*
 
-  import cats._
-  import cats.implicits._
+class MyPropertyTests extends AnyFlatSpec with Matchers with ScalaCheckPropertyChecks:
 
   "An Int" should "combine commutatively" in {
     forAll { (a: Int, b: Int) =>
@@ -34,9 +29,6 @@ class MyPropertyTests extends AnyFlatSpec with Matchers with ScalaCheckPropertyC
       (a |+| a.inverse()) shouldBe Monoid[Int].empty
     }
   }
-
-}
-
 
 
 // eof
